3c8b8091ea06311a952736a521909890966a4d48
oprogramador <uomodislesia@gmail.com>
2015-11-05 17:50:38 +0100

    general refactor

--- World.jl
+++ World.jl
@@ -1,31 +1,7 @@
 module World
-
-type Position
-  x::Float32
-  y::Float32
-end
-
-type Animal
-  force::Float32
-  speed::Float32
-  temperature::Float32
-  position::Position
-end
-
-type Field
-  temperature::Float32
-end
-
-type Board
-  fields::Array{World.Field}
-end
-
-function init(width, height, n)
-  fields = fill(Field(0), width, height)
-  board = Board(fields)
-end
-
-board = init(100, 100, 3)
-  
-
+  include("./Utils.jl")
+  include("./PositionModule.jl")
+  include("./FieldModule.jl")
+  include("./AnimalModule.jl")
+  include("./BoardModule.jl")
 end
